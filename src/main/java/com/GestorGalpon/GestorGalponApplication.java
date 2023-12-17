246bb256904b9453b1b9014db955f46131c4747c
Matias <akucfh@gmail.com>
2023-12-17 12:48:41 -0300

    firstCommit

---
+++ src/main/java/com/GestorGalpon/GestorGalponApplication.java
@@ -0,0 +1,13 @@
+package com.GestorGalpon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorGalponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorGalponApplication.class, args);
+	}
+
+}
