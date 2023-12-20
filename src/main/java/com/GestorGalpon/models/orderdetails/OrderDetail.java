57b082e4d18e276e35e474d06954d328a16ad237
FacundoLB <facuu142@gmail.com>
2023-12-19 23:26:53 -0300

    listo

---
+++ src/main/java/com/GestorGalpon/models/orderdetails/OrderDetail.java
@@ -0,0 +1,40 @@
+package com.GestorGalpon.models.orderdetails;
+
+import java.util.Date;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "order_details")
+@NoArgsConstructor
+public class OrderDetail {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany
+    private List<Product> products;
+
+    @Column(name = "sub_total")
+    private Double subTotal;
+
+    @Column(name = "update_at")
+    private Date updateAt;
+
+    @Column(name = "is_present")
+    private Boolean isPresent;
+
+
+}
