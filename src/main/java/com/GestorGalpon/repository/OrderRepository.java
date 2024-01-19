da35de68feec1dbf96b165c83f56c476017b5082
FacundoLB <facuu142@gmail.com>
2024-01-19 00:25:50 -0300

    rellenando updateOrder

--- src/main/java/com/GestorGalpon/repository/OrderRepository.java
+++ src/main/java/com/GestorGalpon/repository/OrderRepository.java
@@ -2,6 +2,7 @@ package com.GestorGalpon.repository;
 
 
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +13,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.GestorGalpon.models.order.Order;
+import com.GestorGalpon.models.orderdetails.OrderDetail;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order,Long>{
@@ -24,5 +26,8 @@ public interface OrderRepository extends JpaRepository<Order,Long>{
 
     Optional<Order> updateIsPresent(@Param ("id") Long orderId, @Param("isPresent") Boolean isPresent);
 
+@Modifying
+   @Query("UPDATE order o SET o.orderDetail =:orderDetailId, o.Product = :productId, o.itemsNumber = :itemsNumber, o.updateAt = :upDate WHERE od.id = :id and od.isPresent = true")
+    Optional<Order>  updateOrder(@Param("id") Long orderId ,@Param("orderDetailId") Long orderDetailId, @Param("productId") Long productId,@Param("itemsNumber") Integer itemsNumber,@Param("upDate") Date updateAt);
 
 }
--- src/main/java/com/GestorGalpon/service/implementation/OrderServiceImp.java
+++ src/main/java/com/GestorGalpon/service/implementation/OrderServiceImp.java
@@ -2,15 +2,12 @@ package com.GestorGalpon.service.implementation;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.GestorGalpon.models.order.Order;
 import com.GestorGalpon.models.order.dto.RequestOrder;
 import com.GestorGalpon.models.order.dto.ResponseOrder;
-import com.GestorGalpon.repository.OrderDetailRepository;
 import com.GestorGalpon.repository.OrderRepository;
 import com.GestorGalpon.service.abstraction.OrderService;
 
@@ -18,19 +15,17 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
+@Transactional
 public class OrderServiceImp implements OrderService{
 
     private final OrderRepository orderRepository;
-
-    private final OrderDetailRepository orderDetailRepository;
     @Override
     public ResponseOrder createOrder(RequestOrder order) {
         return new ResponseOrder(orderRepository.save(new Order(order)));
     }
 
-   /*@Override
+    @Override
     public ResponseOrder updateIsPresent(Long orderId, Boolean isPresent) {
 
       
@@ -41,7 +36,7 @@ public class OrderServiceImp implements OrderService{
         }
 
 
-    } */ 
+    }
 
     @Override
     public ResponseOrder findById(Long orderId) {
@@ -57,17 +52,7 @@ public class OrderServiceImp implements OrderService{
     @Override
     public ResponseOrder updateOrder(Long orderId, Long orderDetailId, Long productId, Integer itemsNumber) {
 
-        Optional<Order> order = orderRepository.findById(orderId);
-
-        if(order.isPresent()){
-
-            orderDetailRepository.updateOrderDetail(orderDetailId,itemsNumber, new Date());
-            
-        return new ResponseOrder(order.get());            
-        }
-
-        return null;
+        return new ResponseOrder(orderRepository.updateOrder(orderId,orderDetailId,productId,itemsNumber,new Date()));;
     }
-
     
 }
